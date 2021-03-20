96a8842e9fe32bb65d788b949598aa9bb2ad46b7
Sam-Berger <sam.d.berger@gmail.com>
2021-03-19 20:18:48 -0500

    Completed Assignment

--- rover.js
+++ rover.js
@@ -1,5 +1,67 @@
 class Rover {
-   // Write code here!
-}
-
-module.exports = Rover;
+   constructor(position) {
+     this.position = position;
+     this.mode = "NORMAL";
+     this.generatorWatts = 110;
+   }
+ 
+  receiveMessage(message) {
+    let resultsArr = [];
+      
+ 
+     for (let i = 0; i < message.commands.length; i++) {
+       if (message.commands[i] === "STATUS_CHECK") {
+         resultsArr.push(
+          {
+           completed: true,
+           "roverStatus": 
+             {
+             mode: this.mode,
+             generatorWatts: this.generatorWatts,
+             position: this.position
+             }
+          })
+      }
+      else if (message.commands[i].commandType === "MODE_CHANGE"){
+        if (message.commands[i].value === "LOW_POWER") {
+           this.mode = "LOW_POWER"
+        }  
+         if (message.commands[i].value === "NORMAL") {
+           this.mode = "NORMAL"
+         }
+       resultsArr.push({completed: true}) 
+      }
+      else if (message.commands[i].commandType === "MOVE") {
+        if (this.mode === "LOW_POWER") {
+          resultsArr.push({completed: false})
+        }
+        if (this.mode === "NORMAL") {
+          this.position += message.commands[i].value
+        }
+      }
+      
+      //this else should probably not exist and its function should be elsewhere. What happens when removed?
+      else {
+        resultsArr.push({completed: true})
+      }
+     }
+   return {
+     name: message.name,
+     results: resultsArr
+   }
+  }
+ }
+ 
+ /*
+ receiveMessage(message) {
+   let resultsArr = message;
+   //is message passing an array? message sends object, but the commands key has an array as value.
+ //how do I iterate through the length of of array for multiple commands?
+ 
+ //message.commands.value and message.commands/commandType are what I need to solve test 11
+   return resultsArr
+ }
+ */
+ 
+ 
+ module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -1,13 +1,85 @@
-const Rover = require('../rover.js');
-const Message = require('../message.js');
+const assert = require('assert');
 const Command = require('../command.js');
-
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
+const Message = require('../message.js');
+const Rover = require('../rover.js')
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+
+    let test = new Rover(100)
+    assert.strictEqual(test.position, 100);
+    assert.strictEqual(test.mode, "NORMAL");
+    assert.strictEqual(test.generatorWatts, 110)
+  });
+
+  it("response returned by receiveMessage contains name of message", function() {
+
+    let testCommands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let testMessage = new Message('Test message with two commands', testCommands);
+    let test = new Rover(100)
+
+    assert.strictEqual(test.receiveMessage(testMessage).name, 'Test message with two commands')
+  });
+
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+    
+    let testCommands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let testMessage = new Message('Test message with two commands', testCommands);
+    let test = new Rover(100)
+
+    assert.strictEqual(test.receiveMessage(testMessage).results.length, 2)
+  })
+
+  it("responds correctly to status check command", function() {
+    let testRover = new Rover(100);
+    let testMessage = new Message('Check Status', ['STATUS_CHECK'])
+    
+    assert.strictEqual(testRover.receiveMessage(testMessage).results[0].roverStatus.mode, "NORMAL")
+    assert.strictEqual(testRover.receiveMessage(testMessage).results[0].roverStatus.generatorWatts, 110)
+    assert.strictEqual(testRover.receiveMessage(testMessage).results[0].roverStatus.position, 100)
+  })
+
+  it("responds to command change corrently", function() {
+    let testRover = new Rover(100);
+    let testToLowPower = [new Command('MODE_CHANGE', 'LOW_POWER')]
+    let testMessageLow = new Message('Changes to Low Power', testToLowPower);
+    testRover.receiveMessage(testMessageLow)
+
+    assert.strictEqual(testRover.mode, "LOW_POWER")
+    assert.strictEqual(testRover.receiveMessage(testMessageLow).results[0].completed, true)
+
+    let testToNormalPower = [new Command('MODE_CHANGE', 'NORMAL')]
+    let testMessageNormal = new Message('Changes to Normal Power', testToNormalPower);
+
+    assert.strictEqual(testRover.receiveMessage(testMessageNormal).results[0].completed, true)
+
+    testRover.receiveMessage(testMessageNormal)
+    assert.strictEqual(testRover.mode, "NORMAL")
+  })
+  
+  it("responds with false completed value when attempting to move in LOW_POWER mode", function () {
+    let testRover = new Rover(100);
+    let testToLowPower = [new Command('MODE_CHANGE', 'LOW_POWER')]
+    let testMessageLow = new Message('Changes to Low Power', testToLowPower);
+    testRover.receiveMessage(testMessageLow)
+    let lowMoveCommand = [new Command('MOVE', 5)]
+    let lowMoveMessage = new Message('Move in Low Power Mode', lowMoveCommand);
+
+    assert.strictEqual(testRover.receiveMessage(lowMoveMessage).results[0].completed,false)
+
+    assert.strictEqual(testRover.position, 100)
+  })
+
+  it("responds with position for move command", function () {
+    let testRover = new Rover(100);
+    let moveCommand = [new Command('MOVE', 5)]
+    let moveMessage = new Message('Move 5 units in Normal Power Mode', moveCommand);
+
+    testRover.receiveMessage(moveMessage)
+    assert.strictEqual(testRover.position, 105)
+  })
+
 
 });
